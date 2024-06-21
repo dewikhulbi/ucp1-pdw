086792ea44ad257153c996009990b1b21e1948d7
dewikhulbi <dewikhlb20@gmail.com>
2024-06-21 22:21:40 +0700

    first commit

---
+++ flight-booking/js/script.js
@@ -0,0 +1,11 @@
+function loginUser() {
+    // Simulate user login
+    event.preventDefault();
+    window.location.href = "user/dashboard.html";
+}
+
+function loginAdmin() {
+    // Simulate admin login
+    event.preventDefault();
+    window.location.href = "admin/dashboard.html";
+}
---
+++ flight-booking/user/script.js
@@ -0,0 +1,5 @@
+function selectTicket(flightNumber) {
+    alert("Ticket for " + flightNumber + " selected!");
+    // Simulate storing the selected ticket information and navigate to details page
+    window.location.href = "details.html";
+}
